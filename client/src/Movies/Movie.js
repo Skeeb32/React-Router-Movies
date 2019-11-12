2f115678d16aee2c0628ab2b073cb1a61c1e6ebd
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2019-11-12 15:07:56 -0800

    Almost done with Pt. 2

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = Number(props.match.params.id)
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
